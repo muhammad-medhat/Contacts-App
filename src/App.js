5071d0c6e2fc7ad3c6988e1234ef3c10cd582762
Muhammad <Muhammad.medhat@gmail.com>
2022-03-07 16:45:50 +0200

    life cycle events

--- src/App.js
+++ src/App.js
@@ -1,33 +1,48 @@
-import logo from './logo.svg';
+import React, { Component } from 'react';
+
 import './App.css';
 import ListContacts from './ListContacts';
-  const contacts = [
-    {
-      "id": "karen",
-      "name": "Karen Isgrigg",
-      "handle": "karen_isgrigg",
-      "avatarURL": "http://localhost:5001/karen.jpg"
-    },
-    {
-      "id": "richard",
-      "name": "Richard Kalehoff",
-      "handle": "richardkalehoff",
-      "avatarURL": "http://localhost:5001/richard.jpg"
-    },
-    {
-      "id": "tyler",
-      "name": "Tyler McGinnis",
-      "handle": "tylermcginnis",
-      "avatarURL": "http://localhost:5001/tyler.jpg"
-    }
-   ];
-function App() {
+import * as ContactsAPI from './utils/ContactsAPI';
 
-  return (
-    <div className="App">
-      <ListContacts cons={contacts} />
-    </div>
-  );
+class App extends Component {
+  state = {
+    contacts: []
+  };
+  componentDidMount = () => {
+    ContactsAPI.getAll()
+      .then((contacts) => {
+        this.setState(() => {
+          contacts
+        })
+      })
+  }
+  removeContact = (contact) => {
+    this.setState((currentState) => ({
+
+      contacts: currentState.contacts.filter((c) => {
+        return c.id !== contact.id
+      })
+      
+    }))
+  }
+  // removeContact(c){
+  //   this.setState( (currState) =>{
+  //     state.contacts: currState.contacts.filter(con =>{
+  //       return con.id!==c.id
+  //     } )
+  //   })
+  // }
+  render() {
+      return (
+        <div className="App">
+          <ListContacts 
+            cons={this.state.contacts} 
+            onDelete={this.removeContact}
+            />
+        </div>
+    );
+  }
+
 }
 
 export default App;
--- src/ListContacts.js
+++ src/ListContacts.js
@@ -1,18 +1,114 @@
-import React, { Component } from 'react';
-class ListContacts extends Component {
-    state = {  } 
-    render() {             
-        console.log('cons', this.props)
+import React, {Component} from 'react';
+import PropTypes from 'proptypes';
 
-        return (
-            <ul className='contacts-list'>
-                {this.props.cons.map(x=>
-                    <li key={x.id}>{x.name}</li>
-                )}
-                
-            </ul>
+
+class ListContacts extends Component {
+
+    static propTypes = {
+        cons: PropTypes.array.isRequired, 
+        onDelete: PropTypes.func.isRequired
+    }
+    state = { 
+        query: ''
+    } 
+    updateQuery(q){
+        this.setState(
+            ()=>({
+                query: q.trim()
+            })
+        )
+    }
+
+    render() {    
+        const {query} = this.state
+        const { cons, onDelete} = this.props
+        const show = query === '' ? cons: cons.filter(c=> c.name.toLowerCase().includes(query))
+        
+        console.log('cons', this.props)    
+        return ( 
+            <>
+            <div className='list-contacts'>
+                <div className='list-contacts-top'>
+                    <input
+                        className='search-contacts' 
+                        type='text'
+                        placeholder='Search a contact'
+                        value={this.state.query}
+                        onChange={(e)=>{this.updateQuery(e.target.value)}}
+                        
+                        />
+                </div>
+{
+    JSON.stringify(this.state)
+}
+            </div>
+                    <ul className='contact-list'>
+                        {show.map(x=>
+                        <li key={x.id} className='contact-list-item'>     
+                            <div 
+                                className='contact-avatar'
+                                style={{
+                                    backgroundImage:`url(${x.avatarURL})`
+                                }}
+                                >                            
+                            </div>
+
+                            <div className='contact-details'>
+                                <p>{x.name}</p>
+                                <p>{x.handle}</p>
+                            </div>
+                            <button 
+                                className='contact-remove'
+                                onClick={()=>onDelete(x)}>
+                                remove
+                            </button>
+                        </li>
+                        )}
+                    </ul>            
+            </>
+
         );
     }
 }
- 
 export default ListContacts;
+
+
+
+
+
+
+
+
+// class ListContacts extends Component {
+//     state = {  } 
+//     render() {             
+//         console.log('cons', this.props)
+
+//         return (
+//             <ul className='contact-list'>
+//                 {this.props.cons.map(x=>
+//                     <li key={x.id} className='contact-list-item'>     
+//                         <div 
+//                             className='contact-avatar'
+//                             style={{
+//                                 backgroundImage:`url(${x.avatarURL})`
+//                             }}
+//                             >                            
+//                         </div>
+
+//                         <div className='contact-details'>
+//                             <p>{x.name}</p>
+//                             <p>{x.handle}</p>
+//                         </div>
+//                         <button className='contact-remove'>
+//                             remove
+//                         </button>
+//                     </li>
+//                 )}
+                
+//             </ul>
+//         );
+//     }
+// }
+ 
+// export default ListContacts;
